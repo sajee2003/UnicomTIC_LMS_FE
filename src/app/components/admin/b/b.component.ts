e093a84611db6e10a24f1ad6f56c1a24aad9ae86
ut01107 <ut01107@UNICOMTIC11>
2025-03-01 12:31:48 +0530

    Folder Structure Updated

---
+++ src/app/components/admin/b/b.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-b',
+  imports: [],
+  templateUrl: './b.component.html',
+  styleUrl: './b.component.css'
+})
+export class BComponent {
+
+}
