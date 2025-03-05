01bdd37ddcf4c16607bfa9d645b7f037423dfcf3
ut01107 <ut01107@UNICOMTIC11>
2025-03-05 11:25:12 +0530

    sidebar bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { SidebarComponent } from './components/shared/sidebar/sidebar.component'
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/shared/login/login.component';
 import { RegisterStudentComponent } from './components/student/register/register-student/register-student.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar/sidebar.component';
 
 
 @Component({
@@ -14,8 +13,6 @@ import { SidebarComponent } from './components/shared/sidebar/sidebar/sidebar.co
   standalone: true,
 
   imports: [RouterOutlet,LoginComponent,RegisterStudentComponent,SidebarComponent],
-
-
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
