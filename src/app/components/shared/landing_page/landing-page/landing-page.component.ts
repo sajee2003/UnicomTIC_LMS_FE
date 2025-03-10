bc7503256b0638d9a9fe3b5cf2388bd63e368e31
ut01107 <ut01107@UNICOMTIC11>
2025-03-10 12:31:30 +0530

    routing added

--- src/app/components/shared/landing_page/landing-page/landing-page.component.ts
+++ src/app/components/shared/landing_page/landing-page/landing-page.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { faCoffee ,faBrush} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Import the FontAwesomeModule
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-landing-page',
-  imports: [ReactiveFormsModule, RouterModule, FormsModule, CommonModule,FontAwesomeModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, FontAwesomeModule, HttpClientModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
   template: `<i class="fa-brands fa-java"></i>`,
