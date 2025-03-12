038dc7596be6af328471f5972242541990b62261
Selvachandran R. <UT01107@unicomtic.com>
2025-03-12 16:31:29 +0530

    routings updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/shared/login/login.component';
 import { LandingPageComponent } from './components/shared/landing_page/landing-page/landing-page.component';
 import { StudentDashboardComponent } from './components/student/dashboard/student-dashboard/student-dashboard.component';
+import { RegisterComponent } from './components/shared/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent }, // Default route
     { path: 'login', component: LoginComponent },
     { path: 'studentdash', component: StudentDashboardComponent },
+    { path: 'studentregister', component: RegisterComponent },
+
 
 ]  
