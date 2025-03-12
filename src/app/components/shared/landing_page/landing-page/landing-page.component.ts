60f95635c97caeb21d1d26c449eacaba2ff3f9f5
Niruban R. <UT01568@unicomtic.com>
2025-03-12 17:00:32 +0530

    UI for landing page updated

--- src/app/components/shared/landing_page/landing-page/landing-page.component.ts
+++ src/app/components/shared/landing_page/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { faCoffee ,faBrush} from '@fortawesome/free-solid-svg-icons';
@@ -15,7 +15,36 @@ import { HttpClientModule } from '@angular/common/http';
   template: `<i class="fa-brands fa-java"></i>`,
 
 })
-export class LandingPageComponent {
-faCoffee = faCoffee;
+export class LandingPageComponent  implements OnInit, OnDestroy {
+  images: string[] = [
+    '/ai-generated-portrait-of-a-smiling-schoolgirl-using-laptop-in-classroom-at-school-photo\ \(1\).jpg',
+    '/banner_3.png',
+    '/cap2.png',
+  ];
 
+  currentImageIndex: number = 0;
+  intervalId: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy(): void {
+    // Clear the interval when the component is destroyed
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 3000); // Change image every 3 seconds
+  }
+
+  getSliderTransform(): string {
+    return `translateX(-${this.currentImageIndex * 100}%)`; // Slide images horizontally
+  }
 }
